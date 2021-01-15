af072e2ab4c7df3a3899171cec97ded3eab7a661
Todd Walls <t_e_walls@yahoo.com>
2021-01-15 00:39:59 -0700

    This is my second push. I made a few more modifications/additions.

--- Assets/Script/script.js
+++ Assets/Script/script.js
@@ -4,6 +4,9 @@ var generateBtn = document.querySelector("#generate");
 // Declare variables
 var passwordLength = "";
 var lowercaseState = ""; 
+var uppercaseState = "";
+var numericState = "";
+var specialCharacters = "";
 var lowercase = "";
 var uppercase = "";
 var numeric = "";
@@ -25,37 +28,58 @@ function generatePassword () {
 
   // make sure the password is the correct length 
  if (passwordLengthNum < 8) {
-  window.alert("Your password is not long enough. Try again.");
- 
+  passwordlengthNum = window.prompt("Your password is not long enough. Try again.");
 } else if (passwordLengthNum >128) {
-  window.alert("Your password is too long. Try again.");
-}   else {
-  return;
-}
-  
+  passwordlengthNum = window.prompt("Your password is too long. Try again.");
+}   
   // lowercase?
   var lowercaseChoice = window.prompt("Would you like lowercase letters (Y or N)?");
   console.log(lowercase);
   
   if (lowercaseChoice == Y) {
-    lowercaseState === "true"; 
+    lowercaseState === lowerCaseLettersArr; 
   } else if (lowercaseChoice == N) {
-    lowercaseState === "false";
+    lowercaseState === "";
   } else {
-    window.alert("Invalid inout. Type 'Y' for Yes or 'N' for no.");
+    lowercaseChoice = window.prompt("Invalid input. Type 'Y' for Yes or 'N' for no.");
   }
 
   // uppercase?
-  var uppercase = window.prompt("Would you like uppercase letters (Y or N)?");
+  var uppercaseChoice = window.prompt("Would you like uppercase letters (Y or N)?");
   console.log(uppercase);
+
+  if (uppercaseChoice == Y) {
+    uppercaseState === upperCaseLettersArr; 
+  } else if (uppercaseChoice == N) {
+    uppercaseState === "";
+  } else {
+    uppercaseChoice = window.prompt("Invalid input. Type 'Y' for Yes or 'N' for no.");
+  }
 
   // numeric?
   var numeric = window.prompt("Would you like numeric values (Y or N)?");
   console.log(numeric);
+  
+  if (numeric == Y) {
+    numericState === numericalCharsArr; 
+  } else if (numeric == N) {
+    numericState === "";
+  } else {
+    numericState = window.prompt("Invalid input. Type 'Y' for Yes or 'N' for no.");
+  }
+
 
   // special character?
   var specialCharacters = window.prompt("Would you like special characters (Y or N)?");
   console.log(specialCharacters);
+
+  if (specialCharacters == Y) {
+    specialCharactersState === "specialCharsArr"; 
+  } else if (lowercaseChoice == N) {
+    lowercaseState === "false";
+  } else {
+    lowercaseChoice = window.prompt("Invalid inout. Type 'Y' for Yes or 'N' for no.");
+  }
 
 }
 
@@ -79,24 +103,3 @@ generateBtn.addEventListener("click", writePassword);
 // when all prompts are answered, a password is generated that matches all the selected criteria
 // WHEN the password is generated -
 // THEN the password is either displayed in an alert or written to the page
-
-
-// ## Acceptance Criteria
-
-// ```
-// GIVEN I need a new, secure password
-// WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-// WHEN prompted for character types to include in the password
-// THEN I choose lowercase, uppercase, numeric, and/or special characters
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
-// 
