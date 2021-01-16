376fbf0e5dac161f5bbaa9ef2e11d128eec69bd3
Todd Walls <t_e_walls@yahoo.com>
2021-01-15 23:00:03 -0700

    I cleaned up the validation - it works better but still not perfect. After meeting with tutor, he helped me clean up syntax to more easily create an array of characters user chooses. I started a for loop to create the password to the correct length, but when I run the program I am told the array.push() is not a valid function and I can not figure out why. Back at it tomorrow.

--- Assets/Script/script.js
+++ Assets/Script/script.js
@@ -2,15 +2,10 @@
 var generateBtn = document.querySelector("#generate");
 
 // Declare variables
-var passwordLength = "";
-var lowercaseState = ""; 
-var uppercaseState = "";
-var numericState = "";
-var specialCharacters = "";
-var lowercase = "";
-var uppercase = "";
-var numeric = "";
-var specialCharacters = "";
+var passwordLength ="";
+var characterChoices = [];
+var passwordArr = [];
+var passwordSt = ""
 const lowerCaseLettersArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 const upperCaseLettersArr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 const numericalCharsArr = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
@@ -19,68 +14,93 @@ const specialCharsArr = [' ', '!', '#', '$', '%', '&', '(', ')', '*', '+', ',', 
 // create a function called generatePassword() that prompts the user - what character types would you like? 
 function generatePassword () {
   // how long would you like your password? (min 8/max128)?
-  var passwordLengthSt = window.prompt("How long would you like your password? (min 8/max128)?");
+  var passwordLengthSt = window.prompt("How long would you like your password? (min 8 characters/max 128 characters)?");
   console.log(passwordLengthSt);
 
   // convert passwordLength from string to number
  var passwordLengthNum = parseInt(passwordLengthSt);
- console.log(password);
+ console.log(passwordLengthNum);
 
   // make sure the password is the correct length 
  if (passwordLengthNum < 8) {
-  passwordlengthNum = window.prompt("Your password is not long enough. Try again.");
-} else if (passwordLengthNum >128) {
-  passwordlengthNum = window.prompt("Your password is too long. Try again.");
-}   
+  passwordlengthNum = window.prompt("Your password length has too few characters. Try again.");
+ } 
+ else if (passwordLengthNum > 128) {
+  passwordlengthNum = window.prompt("Your password length has too many characters. Try again.");
+ } 
+ else {
+ }
+
   // lowercase?
   var lowercaseChoice = window.prompt("Would you like lowercase letters (Y or N)?");
-  console.log(lowercase);
-  
-  if (lowercaseChoice == Y) {
-    lowercaseState === lowerCaseLettersArr; 
-  } else if (lowercaseChoice == N) {
-    lowercaseState === "";
-  } else {
+  console.log(lowercaseChoice);
+
+  // validate choice
+  if (lowercaseChoice == "Y") {
+    characterChoices = characterChoices.concat(lowerCaseLettersArr); 
+  } 
+  else if (!lowercaseChoice == "N" && !lowercaseChoice == "Y") 
+  {
     lowercaseChoice = window.prompt("Invalid input. Type 'Y' for Yes or 'N' for no.");
-  }
+  } 
+  else if (lowercaseChoice == "N"){ 
+  } 
 
   // uppercase?
   var uppercaseChoice = window.prompt("Would you like uppercase letters (Y or N)?");
-  console.log(uppercase);
+  console.log(uppercaseChoice);
 
-  if (uppercaseChoice == Y) {
-    uppercaseState === upperCaseLettersArr; 
-  } else if (uppercaseChoice == N) {
-    uppercaseState === "";
-  } else {
-    uppercaseChoice = window.prompt("Invalid input. Type 'Y' for Yes or 'N' for no.");
+  // validate choice
+  if (uppercaseChoice == "Y") {
+    characterChoices = characterChoices.concat(upperCaseLettersArr); 
+  } 
+  else if (!uppercaseChoice == "N" && !uppercaseChoice == "Y") 
+  {
+      uppercaseChoice = window.prompt("Invalid input. Type 'Y' for Yes or 'N' for no.");
+  } 
+  else if (uppercaseChoice == "N"){    
   }
 
   // numeric?
-  var numeric = window.prompt("Would you like numeric values (Y or N)?");
-  console.log(numeric);
-  
-  if (numeric == Y) {
-    numericState === numericalCharsArr; 
-  } else if (numeric == N) {
-    numericState === "";
-  } else {
-    numericState = window.prompt("Invalid input. Type 'Y' for Yes or 'N' for no.");
-  }
+  var numericChoice = window.prompt("Would you like numeric values (Y or N)?");
+  console.log(numericChoice);
 
+  // validate choice
+  if (numericChoice == "Y") {
+    characterChoices = characterChoices.concat(numericalCharsArr);
+  } 
+  else if (!numericChoice == "N" && !numericChoice == "Y") {
+      numericChoice = window.prompt("Invalid input. Type 'Y' for Yes or 'N' for no.");
+  } 
+  else if (numericChoice == "N") {    
+  }
 
   // special character?
-  var specialCharacters = window.prompt("Would you like special characters (Y or N)?");
-  console.log(specialCharacters);
+  var specialCharChoice = window.prompt("Would you like special characters (Y or N)?");
+  console.log(specialCharChoice);
 
-  if (specialCharacters == Y) {
-    specialCharactersState === "specialCharsArr"; 
-  } else if (lowercaseChoice == N) {
-    lowercaseState === "false";
-  } else {
-    lowercaseChoice = window.prompt("Invalid inout. Type 'Y' for Yes or 'N' for no.");
+  // validate choice
+  if (specialCharChoice == "Y") {
+    characterChoices = characterChoices.concat(specialCharsArr);
+  } 
+  else if (!numericChoice == "N" && !numericChoice == "Y") {
+      specialCharChoice = window.prompt("Invalid input. Type 'Y' for Yes or 'N' for no.");
+  } 
+  else if (specialCharChoice == "N") {
   }
+  console.log(characterChoices);
 
+
+  for (i = 0 ; i = passwordLengthNum ; i++) {
+      var selectIndex = Math.floor(Math.random() * (characterChoices.length));
+      console.log(selectIndex);
+      var charChoiceSt = characterChoices[selectIndex];
+      passwordArr = passwordArr.push(charChoiceSt);
+      // passwordArr = passwordArr.push(characterChoices[selectIndex].toString());
+      passwordSt = passwordArr.toString();
+      console.log(passwordSt); 
+  }
+  return passwordSt;
 }
 
 // Write password to the #password input
